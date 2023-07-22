caa8ec8517fdf516b4c6b9debb0e8137b46d3fc7
i-xi-dev <naoyuki.isono@gmail.com>
2023-07-22 12:52:43 +0900

    4.0.18

--- build_npm.ts
+++ build_npm.ts
@@ -15,7 +15,7 @@ await build({
   rootTestDir: "./tests",
   package: {
     name: "@i-xi-dev/percent",
-    version: "4.0.17",
+    version: "4.0.18",
     description: "This is not for direct usage.",
     license: "MIT",
     author: "i-xi-dev",
