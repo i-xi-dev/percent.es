10c220c9bbd18c8e8221199640f367468fbc7ffa
i-xi-dev <naoyuki.isono@gmail.com>
2023-09-11 23:38:30 +0900

    4.0.24

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export { Uint8 } from "https://raw.githubusercontent.com/i-xi-dev/number.es/3.0.0/mod.ts";
-export { BytesEncoding } from "https://raw.githubusercontent.com/i-xi-dev/bytes-encoding.es/1.0.17/mod.ts";
+export { Uint8 } from "https://raw.githubusercontent.com/i-xi-dev/number.es/3.0.1/mod.ts";
+export { BytesEncoding } from "https://raw.githubusercontent.com/i-xi-dev/bytes-encoding.es/1.0.18/mod.ts";
--- tests/deps.ts
+++ tests/deps.ts
@@ -1,4 +1,4 @@
 export {
   assertStrictEquals,
   assertThrows,
-} from "https://deno.land/std@0.198.0/assert/mod.ts";
+} from "https://deno.land/std@0.201.0/assert/mod.ts";
