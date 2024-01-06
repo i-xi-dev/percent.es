4a1338dac477c420b40cbbf350ed25d09a0e9518
i-xi-dev <naoyuki.isono@gmail.com>
2024-01-07 00:27:28 +0900

    4.0.30

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export { Uint8 } from "https://raw.githubusercontent.com/i-xi-dev/number.es/5.3.0/mod.ts";
-export { BytesEncoding } from "https://raw.githubusercontent.com/i-xi-dev/bytes-encoding.es/1.0.22/mod.ts";
+export { Uint8 } from "https://raw.githubusercontent.com/i-xi-dev/number.es/5.4.2/mod.ts";
+export { BytesEncoding } from "https://raw.githubusercontent.com/i-xi-dev/bytes-encoding.es/1.0.23/mod.ts";
