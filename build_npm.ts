f3529c9d62b9883c8a72bdcaea5d1fd409b44e16
i-xi-dev <naoyuki.isono@gmail.com>
2024-04-19 01:03:01 +0900

    4.0.36

--- build_npm.ts
+++ build_npm.ts
@@ -15,7 +15,7 @@ await build({
   rootTestDir: "./tests",
   package: {
     name: "@i-xi-dev/percent",
-    version: "4.0.35",
+    version: "4.0.36",
     description: "This is not for direct usage.",
     license: "MIT",
     author: "i-xi-dev",
@@ -31,10 +31,10 @@ await build({
     publishConfig: {
       access: "public",
     },
-    files: [
-      "esm",
-      "types",
-    ],
+    // files: [
+    //   "esm",
+    //   "types",
+    // ],
   },
   typeCheck: "both",
   declaration: "inline",
--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export { BytesEncoding } from "https://raw.githubusercontent.com/i-xi-dev/bytes-encoding.es/1.0.28/mod.ts";
-export { Uint8 } from "https://raw.githubusercontent.com/i-xi-dev/number.es/7.3.0/mod.ts";
+export { BytesEncoding } from "https://raw.githubusercontent.com/i-xi-dev/bytes-encoding.es/1.0.29/mod.ts";
+export { Uint8 } from "https://raw.githubusercontent.com/i-xi-dev/number.es/8.0.0/mod.ts";
